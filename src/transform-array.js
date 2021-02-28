af89abb0483d878de2bdc7dcc02923426b7cf365
akool <akoolny@gmail.com>
2021-02-28 23:43:30 +0300

    feat: add interim solution for "Transform array"

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('Argument is not Array');
+  
+  let result = [];
+  const controls = ['--double-next', '--double-prev', '--discard-next', '--discard-prev'];
+
+  arr.forEach((item, i) => {
+    if (controls.includes(item)) return;
+    
+    if (arr[i - 1] === '--double-next') {
+      result.push(item, item);
+      return;
+    } else {
+      result.push(item);
+    }
+    
+    if (arr[i + 1] === '--double-prev') {
+      result.push(item);
+      return;
+    }
+    
+    if (arr[i - 1] === '--discard-next') {
+      result.push(item);
+      return;
+    }
+    
+    if (arr[i + 1] === '--discard-prev') {
+      result.pop();
+      return;
+    }    
+    
+  });
+
+  return result;
 };
