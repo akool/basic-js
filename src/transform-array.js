3208f7254db59b937aa10dd6ab228303f3816e94
akool <akoolny@gmail.com>
2021-03-02 00:26:32 +0300

    feat: remake solution for "Transform array"

--- src/transform-array.js
+++ src/transform-array.js
@@ -6,32 +6,27 @@ module.exports = function transform(arr) {
   let result = [];
   const controls = ['--double-next', '--double-prev', '--discard-next', '--discard-prev'];
 
-  arr.forEach((item, i) => {
-    if (controls.includes(item)) return;
-    
-    if (arr[i - 1] === '--double-next') {
-      result.push(item, item);
-      return;
-    } else {
-      result.push(item);
-    }
-    
-    if (arr[i + 1] === '--double-prev') {
-      result.push(item);
-      return;
-    }
+  return arr.reduce((acc, cur, i) => {
+
+    if (controls.includes(cur)) return acc;
     
     if (arr[i - 1] === '--discard-next') {
-      result.push(item);
-      return;
+      return acc;
+    }
+    
+    if (arr[i - 1] === '--double-next') {
+      acc.push(cur, cur);
+    } else {
+      acc.push(cur);
+    }
+    
+    if (arr[i + 1] === '--double-prev') {  
+      acc.push(cur);
     }
     
     if (arr[i + 1] === '--discard-prev') {
-      result.pop();
-      return;
-    }    
-    
-  });
-
-  return result;
+      return acc.slice(0, -1);
+    }   
+    return acc;
+  }, []);
 };
