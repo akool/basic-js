1ab193e498d68cf7953b0b050dcaf807c6cd8241
akool <akoolny@gmail.com>
2021-03-01 22:11:05 +0300

    feat: add solution for "Chain maker"

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+  separator: '~~',
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(`( ${String(value)} )`);
+    
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.chain[position-1]) {
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position-1, 1);
+    
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.chain.join(this.separator);
+    this.chain = [];
+    return result;
   }
 };
 
